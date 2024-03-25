b769b1d18a88b83ab26a966ad3fd9c112545e77d
Damilola Olawoore <Htcode@Oluwaseyis-MacBook-Pro.local>
2024-03-25 10:00:06 +0100

    Extended the token expiry date and fixed the corsheader

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -92,6 +92,11 @@ CORS_ALLOWED_ORIGINS = [
 
 ]
 
+CORS_ORIGIN_WHITELIST = [
+    "https://dammy-todoapp.netlify.app",
+    "https://htcode12.pythonanywhere.com",
+]
+
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
@@ -104,7 +109,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=90),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     
     }
